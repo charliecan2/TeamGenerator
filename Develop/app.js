4b4dcd35f24bc608e6166fb26469c232a10fd763
charliecan2 <carloscq920@gmail.com>
2021-01-28 00:46:00 -0600

    Create three seperate functions that will initialize a series of questions depending on what kind of member is being added to the team

---
+++ Develop/app.js
@@ -0,0 +1,118 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+
+const teamMembers = [];
+
+function manager (){
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the name of your manager?",
+            name: "managerName"
+        },
+        {
+            type: "input",
+            message: "What is your manager's ID?",
+            name: "managerID"
+        },
+        {
+            type: "input",
+            message: "What is your manager's email?",
+            name: "managerEmail"
+        },
+        {
+            type: "input",
+            message: "What is your manager's office number?",
+            name: "officeNumber"
+        },
+    ]).then(response => {
+        const manager = new Manager(response.managerName, response.managerID, )
+        teamMembers.push();
+    })
+}
+function engineer (){
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the name of your engineer?",
+            name: "engineerName"
+        },
+        {
+            type: "input",
+            message: "What is your engineer's ID?",
+            name: "engineerID"
+        },
+        {
+            type: "input",
+            message: "What is your engineer's email?",
+            name: "engineerEmail"
+        },
+        {
+            type: "input",
+            message: "What is your engineer's github username?",
+            name: "github"
+        },
+    ]).then(response => {
+        const engineer = new Engineer(response.engineerName, response.engineerID, response.engineerEmail, response.github)
+        teamMembers.push(engineer);
+    })
+}
+function intern (){
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the name of your intern?",
+            name: "internName"
+        },
+        {
+            type: "input",
+            message: "What is your intern's ID?",
+            name: "internID"
+        },
+        {
+            type: "input",
+            message: "What is your intern's email?",
+            name: "internEmail"
+        },
+        {
+            type: "input",
+            message: "What university does your intern attend?",
+            name: "school"
+        },
+    ]).then(response => {
+        const intern = new Intern(response.internName, response.internID, response.internEmail, response.school)
+        teamMembers.push(intern);
+    })
+}
+
+// Write code to use inquirer to gather information about the development team members,
+// and to create objects for each team member (using the correct classes as blueprints!)
+
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
+
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not.
+
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
+
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+// and Intern classes should all extend from a class named Employee; see the directions
+// for further information. Be sure to test out each class and verify it generates an
+// object with the correct structure and methods. This structure will be crucial in order
+// for the provided `render` function to work! ```
---
+++ Develop/lib/Employee.js
@@ -0,0 +1,11 @@
+// TODO: Write code to define and export the Employee class
+class Employee {
+    constructor (name, id, email, info){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.info = info
+    }
+}
+
+module.exports = Employee;
